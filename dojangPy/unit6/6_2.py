b4840d465aece320976e46b271a554210c4a99d4
Zagoshipda <john713@hanmail.net>
2019-05-13 23:19:52 +0900

    6.2 calculation using variables

---
+++ dojangPy/unit6/6_2.py
@@ -0,0 +1,20 @@
+# 6.2 변수로 계산하기 
+
+a = 10
+b = 20
+c = a+b
+print(c)    # 30 
+
+
+print(a+20) # 30
+print(a)    # 10
+a = a + 20
+print(a)    # 30
+a -= 20
+print(a)    # 10
+
+# 부호 붙이기 
+x = -10
+print(x)    # -10
+print(+x)   # -10
+print(-x)   # 10
