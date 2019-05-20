335b31caa01d5b42be6a33905311e8ca11f2f94d
Zagoshipda <john713@hanmail.net>
2019-05-20 16:51:30 +0900

    15.1 if, elif, else

---
+++ dojangPy/unit15/15_1.py
@@ -0,0 +1,30 @@
+# unit15. elif를 사용하여 여러 방향으로 분기하기 
+
+# (1) if, elif, else 줄 끝에 :(콜론)을 붙인다 (2) 그 다음 줄에서는 반드시 들여쓰기를 한다
+
+
+# elif 사용하기
+
+x = 20
+if x == 10:
+    print('x is 10')
+elif x == 20:
+    print('x is 20')    # True
+
+y = 10
+if y == 10:
+    print('x is 10')    # True
+elif y == 20:
+    print('x is 20')
+
+
+# if, elif, else
+
+x = 30
+if x == 10:
+    print('x is 10')
+elif x == 20:
+    print('x is 20')
+else:
+    print('x is neither 10 nor 20')   #True
+
